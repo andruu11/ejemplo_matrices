bb9136a85dcac6aa9493b4a1e5469c0032fb44a3
Jhass11 <jhass11_candia_valdez@outlook.com>
2017-10-25 17:52:04 -0400

    Ejemplo matrices 1111

---
+++ matriz1111/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca cantidad de filas y columnas:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrizb = new int[x,x];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (i == j)
+                    {
+                        matrizb[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrizb[i, j] = 0;
+                    }
+                }
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write(matrizb[i, j] + " ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
